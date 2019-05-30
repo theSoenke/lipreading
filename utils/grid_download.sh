4612897967784a7fae09079d750ee6295d988fc8
theSoenke <soenke.behrendt@gmail.com>
2019-05-30 20:17:16 +0200

    Add GRID data downloader

---
+++ utils/grid_download.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p data/grid/{videos,aligns}
+for i in {1..34}; do
+    if [[ $i == 21 ]]; then
+        continue
+    fi
+
+    wget http://spandh.dcs.shef.ac.uk/gridcorpus/s$i/video/s$i.mpg_vcd.zip
+    unzip s$i.mpg_vcd.zip
+    mv s$i data/grid/videos/s$i
+    rm s$i.mpg_vcd.zip
+
+    wget http://spandh.dcs.shef.ac.uk/gridcorpus/s$i/align/s$i.tar
+    tar -xf s$i.tar
+    mv align data/grid/aligns/s$i
+    rm s$i.tar
+done
