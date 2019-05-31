72b228eaf4e7c611a504f3178133ef823be306e8
theSoenke <soenke.behrendt@gmail.com>
2019-05-31 21:35:29 +0200

    Add data download scripts

---
+++ scripts/lrs2_download.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+urls=(
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data2/lrs2_v1_partaa"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data2/lrs2_v1_partab"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data2/lrs2_v1_partac"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data2/lrs2_v1_partad"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data2/lrs2_v1_partae"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data2/pretrain.txt"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data2/train.txt"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data2/val.txt"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data2/test.txt"
+)
+
+dir=data/lrs2
+mkdir -p dir
+for url in "${urls[@]}"; do
+    wget --user $USER --password $PASSWORD -P $dir $url
+done
+
+cat $dir/lrs2_v1_parta* > $dir/lrs2_v1.tar
---
+++ scripts/lrs3_download.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+urls=(
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data3/lrs3_pretrain_partaa"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data3/lrs3_pretrain_partab"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data3/lrs3_pretrain_partac"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data3/lrs3_pretrain_partad"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data3/lrs3_pretrain_partae"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data3/lrs3_pretrain_partaf"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data3/lrs3_pretrain_partag"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data3/lrs3_trainval.zip"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data3/lrs3_test_v0.4.zip"
+)
+
+dir=data/lrs3
+mkdir -p dir
+for url in "${urls[@]}"; do
+    wget --user $USER --password $PASSWORD -P $dir $url
+done
+
+cat $dir/lrs3_pretrain_part* > $dir/lrs3_pretrain.zip
