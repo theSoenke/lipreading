a61571508f22912686fe1698c85ced9bdb98209b
theSoenke <soenke.behrendt@gmail.com>
2019-06-17 22:26:37 +0200

    Add LRW download script

---
+++ scripts/lrw_download.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+urls=(
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partaa"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partab"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partac"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partad"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partae"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partaf"
+    "http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partag"
+)
+
+dir=data/lrw
+mkdir -p dir
+for url in "${urls[@]}"; do
+    wget --user $USER --password $PASSWORD -P $dir $url
+done
+
+cat $dir/lrsw-v1-parta* > $dir/lrw-v1.tar
