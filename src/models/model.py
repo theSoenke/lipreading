d05fa318cefa360a38a61798a3ec2d3c9e2fe266
theSoenke <soenke.behrendt@gmail.com>
2019-07-03 12:24:41 +0200

    Add temporal conv as backend

--- src/models/model.py
+++ src/models/model.py
@@ -1,4 +1,5 @@
 from torch import nn
+from torch.functional import F
 
 from src.models.resnet import ResNetModel
 
@@ -37,12 +38,52 @@ class Model(nn.Module):
         )
         self.fc = nn.Linear(256 * 2, num_classes)
         self.softmax = nn.LogSoftmax(dim=2)
-        self.loss = NLLSequenceLoss()
+        # self.loss = NLLSequenceLoss()
+        self.loss = nn.CrossEntropyLoss()
+        self.backend = ConvBackend(num_classes)
 
     def forward(self, x):
         x = self.frontend(x)
         x = self.resnet(x)
-        x, _ = self.lstm(x)
-        x = self.fc(x)
-        x = self.softmax(x)
+        x = self.backend(x)
+        # x, _ = self.lstm(x)
+        # x = self.fc(x)
+        # x = self.softmax(x)
         return x
+
+
+class ConvBackend(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+
+        bn_size = 256
+        self.conv1 = nn.Conv1d(bn_size, 2 * bn_size, 2, 2)
+        self.norm1 = nn.BatchNorm1d(bn_size * 2)
+        self.pool1 = nn.MaxPool1d(2, 2)
+
+        self.conv2 = nn.Conv1d(2 * bn_size, 4 * bn_size, 2, 2)
+        self.norm2 = nn.BatchNorm1d(bn_size * 4)
+
+        self.linear = nn.Linear(4*bn_size, bn_size)
+        self.norm3 = nn.BatchNorm1d(bn_size)
+        self.linear2 = nn.Linear(bn_size, num_classes)
+
+        self.loss = nn.CrossEntropyLoss()
+
+    def forward(self, input):
+        transposed = input.transpose(1, 2).contiguous()
+
+        output = self.conv1(transposed)
+        output = self.norm1(output)
+        output = F.relu(output)
+        output = self.pool1(output)
+        output = self.conv2(output)
+        output = self.norm2(output)
+        output = F.relu(output)
+        output = output.mean(2)
+        output = self.linear(output)
+        output = self.norm3(output)
+        output = F.relu(output)
+        output = self.linear2(output)
+
+        return output
