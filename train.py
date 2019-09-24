c1947a37138cc09f1419f0df1cf296c1f815d49d
theSoenke <soenke.behrendt@gmail.com>
2019-09-24 12:00:00 +0200

    Print initial validation value

--- train.py
+++ train.py
@@ -35,24 +35,23 @@ args = parser.parse_args()
 batch_size = args.batch_size
 epochs = args.epochs
 log_interval = args.log_interval
+pretrained = False if args.checkpoint != None else args.pretrained
+current_time = datetime.now().strftime('%b%d_%H-%M-%S')
 
 torch.manual_seed(42)
 np.random.seed(42)
 # torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.benchmark = False # test
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 query = None
-train_data = LRWDataset(directory=args.data, num_words=args.words)
+train_data = HDF5Dataset(path=args.data, columns=['frames', 'label', 'yaw'], query=query)
 train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size, num_workers=args.workers, pin_memory=True)
-val_data = LRWDataset(directory=args.data, mode='val', num_words=args.words)
+val_data = HDF5Dataset(path=args.data, table='val', columns=['frames', 'label', 'yaw'], query=query)
 val_loader = DataLoader(val_data, shuffle=False, batch_size=batch_size * 2, num_workers=args.workers,)
 samples = len(train_data)
-os.makedirs(args.checkpoint_dir, exist_ok=True)
 
-current_time = datetime.now().strftime('%b%d_%H-%M-%S')
 writer = SummaryWriter(log_dir=os.path.join(args.tensorboard_logdir, current_time))
-pretrained = False if args.checkpoint != None else args.pretrained
 model = Model(num_classes=args.words,  resnet_layers=args.resnet, resnet_pretrained=pretrained).to(device)
 wandb.init(project="lipreading")
 wandb.config.update(args)
@@ -152,6 +151,9 @@ trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
 print(f"Trainable parameters: {trainable_params}")
 start_time = time.time()
 best_val_acc = 0
+os.makedirs(args.checkpoint_dir, exist_ok=True)
+val_acc = validate(0) # log initial val acc
+wandb.log({"best_val_acc": best_val_acc})
 for epoch in range(epochs):
     train(epoch, start_time)
     val_acc = validate(epoch)
