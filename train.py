4bfcf6182e46712ad1ffb439009fb3174773cc15
theSoenke <soenke.behrendt@gmail.com>
2019-10-09 13:32:01 +0200

    Auto set number of workers

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import argparse
 
+import psutil
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import EarlyStopping
 
@@ -18,7 +19,7 @@ if __name__ == "__main__":
     parser.add_argument("--weight_decay", type=float, default=1e-5)
     parser.add_argument("--words", type=int, default=10)
     parser.add_argument("--query", type=str, default=None)
-    parser.add_argument("--workers", type=int, default=4)
+    parser.add_argument("--workers", type=int, default=None)
     parser.add_argument("--resnet", type=int, default=18)
     parser.add_argument("--pretrained", default=True, type=lambda x: (str(x).lower() == 'true'))
     parser.add_argument("--seed", type=int, default=42)
@@ -43,6 +44,7 @@ if __name__ == "__main__":
 
     query = None if args.query == None else [float(x) for x in args.query.split(",")]
     assert query == None or len(query) == 2, "--query param not in format -20,20"
+    args.workers = psutil.cpu_count(logical=False) if args.workers == None else args.workers
     args.pretrained = False if args.checkpoint != None else args.pretrained
     model = LRWModel(
         hparams=args,
