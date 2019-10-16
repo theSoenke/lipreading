3ce7a3dd2fbdc10b9eaadc847ac27a1046fae22c
theSoenke <soenke.behrendt@gmail.com>
2019-10-16 20:13:11 +0200

    Cleanup checkpoints during training

--- scripts/train_experts.sh
+++ scripts/train_experts.sh
@@ -10,26 +10,25 @@ if [[ $data == "" ]]; then
   exit
 fi
 
-# WAND_MODE=dryrun
-
 source activate lipreading
 rm -rf data/checkpoints/lrw/lrw_*.ckpt
 
 trap "exit" INT
 for seed in {0..10}; do
-    echo "Train with seed: ${seed}"
+    echo "\nTrain with seed: ${seed}"
     python3 train.py --data $data --words 10 --seed $seed --batch_size $batch_size
     mv data/checkpoints/lrw/lrw_*.ckpt data/checkpoints/lrw/best_seed_${seed}.pkl
 
     for query in "-90,-20" "-20,20" "20,90"; do
-        echo "Train expert for query: ${query}"
+        echo "\nTrain expert for query: ${query}"
         python3 train.py --data $data --words 10 --seed $seed --query " ${query}" --checkpoint data/checkpoints/lrw/best_seed_${seed}.pkl --batch_size $batch_size
         mv data/checkpoints/lrw/lrw_*.ckpt data/checkpoints/lrw/expert_seed_${seed}_${query}.pkl
     done
 
-    echo "Train attention layer"
+    echo "\nTrain attention layer"
     python3 train_attn.py --data $data --words 10 --seed $seed --batch_size $batch_size \
         --checkpoint_left data/checkpoints/lrw/expert_seed_${seed}_-90,-20.pkl \
         --checkpoint_center data/checkpoints/lrw/expert_seed_${seed}_-20,20.pkl \
         --checkpoint_right data/checkpoints/lrw/expert_seed_${seed}_20,90.pkl
+    rm -rf data/checkpoints/lrw/lrw_*.ckpt
 done
