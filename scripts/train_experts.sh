625b80192a395109d4716330e115f9bd5968e472
theSoenke <soenke.behrendt@gmail.com>
2019-10-31 11:02:45 +0100

    Add early attention training to script

--- scripts/train_experts.sh
+++ scripts/train_experts.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 data=$1
-batch_size=${2:-64}
+batch_size=${2:-24}
 
 if [[ $data == "" ]]; then
   echo "Data path missing"
@@ -14,7 +14,7 @@ source activate lipreading
 rm -rf data/checkpoints/lrw/lrw_*.ckpt
 
 trap "exit" INT
-for seed in {0..10}; do
+for seed in {0..30}; do
     printf "\nTrain with seed: ${seed}"
     python3 train.py --data $data --words 10 --seed $seed --batch_size $batch_size
     mv data/checkpoints/lrw/lrw_*.ckpt data/checkpoints/lrw/best_seed_${seed}.pkl
@@ -30,5 +30,12 @@ for seed in {0..10}; do
         --checkpoint_left data/checkpoints/lrw/expert_seed_${seed}_-90,-20.pkl \
         --checkpoint_center data/checkpoints/lrw/expert_seed_${seed}_-20,20.pkl \
         --checkpoint_right data/checkpoints/lrw/expert_seed_${seed}_20,90.pkl
+
+    printf "\nTrain early attention layer"
+    python3 train_early_attn.py --data $data --words 10 --seed $seed --batch_size $batch_size \
+        --checkpoint_left data/checkpoints/lrw/expert_seed_${seed}_-90,-20.pkl \
+        --checkpoint_center data/checkpoints/lrw/expert_seed_${seed}_-20,20.pkl \
+        --checkpoint_right data/checkpoints/lrw/expert_seed_${seed}_20,90.pkl
+
     rm -rf data/checkpoints/lrw/lrw_*.ckpt
 done
